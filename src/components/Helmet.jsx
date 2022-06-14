33a4e35d9bb8fe2da38839d6777bb9920696a5ef
vuvantuyen441 <vuvantuyen@gmail.com>
2022-06-14 16:14:45 +0700

    update helmet

--- src/components/Helmet.jsx
+++ src/components/Helmet.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 const Helmet = (props) => {
 
     document.title = 'Yolo - ' + props.title
+
+    React.useEffect(() => {
+        window.scrollTo(0,0)
+    }, [])
+    
     return (
         <div>
             {props.children}
